cf90d37e489b4b2db8aa724dddab6278382d2461
EttoreScolar <ettore.scolar98@gmail.com>
2021-10-26 00:56:15 -0300

    Add linearSearch

--- Searching Algorithms/linearSearch.rb
+++ Searching Algorithms/linearSearch.rb
@@ -1,1 +1,13 @@
 
+array = [1,20,35,44,51,65,70,81,911,10]
+
+x = 10
+
+def linearSearch element, num
+    for i in 0..element.size
+        return i if num == element[i]
+    end
+    return -1
+end
+
+puts linearSearch array, x

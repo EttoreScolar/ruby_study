85f00417d9ee169bd7c96340227f7c2d9a88cb83
EttoreScolar <ettore.scolar98@gmail.com>
2021-10-26 01:51:01 -0300

    Add binarySearch

---
+++ Searching Algorithms/binarySearch.rb
@@ -0,0 +1,22 @@
+
+array = [1,2,3,4,5,6,7,8,9,10]
+
+x = 10
+
+
+def binarySearch element, num
+    max = element.size - 1
+    for i in 0..element.size
+        return i if element[i] == num
+        if element[i] > num 
+            max = max + 1
+        else
+            i = max - 1
+        end 
+        i = (i+max)/2
+    end
+
+    return -1
+end
+
+puts binarySearch array, x

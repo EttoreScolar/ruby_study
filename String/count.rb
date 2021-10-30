5f2255b5f6a3a865bd66a8f9080fe8915d1becdc
EttoreScolar <ettore.scolar98@gmail.com>
2021-10-29 21:32:28 -0300

    countCaracter

---
+++ String/count.rb
@@ -0,0 +1,25 @@
+string = '# GeeKs01fOr @ gEEks07'
+
+def countCaracter str 
+
+    maiuscula = 0
+    minuscula = 0
+    numero = 0
+    especial = 0
+
+    for i in 0..(str.size-1)
+        if str[i].ord >= 65 && str[i].ord <= 90
+            maiuscula += 1
+        elsif str[i].ord >= 97 && str[i].ord <= 122
+            minuscula += 1
+        elsif str[i].ord >= 48 && str[i].ord <= 57
+            numero += 1
+        else
+            especial += 1
+        end
+    end
+
+    puts maiuscula, minuscula, numero, especial
+end
+
+countCaracter string

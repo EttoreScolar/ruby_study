89911108792f930fae0ca0b00e2179a10e4f0bfa
EttoreScolar <ettore.scolar98@gmail.com>
2021-11-01 22:11:12 -0300

    refactor linearSearch

--- SearchingAlgorithms/linearSearch.rb
+++ SearchingAlgorithms/linearSearch.rb
@@ -2,12 +2,13 @@
 array = [1,20,35,44,51,65,70,81,911,10]
 
 x = 10
-
-def linearSearch element, num
-    for i in 0..element.size
-        return i if num == element[i]
+class LinearSeach
+    def linearSearch element, num
+        for i in 0..element.size
+            return i if num == element[i]
+        end
+        return -1
     end
-    return -1
 end
 
-puts linearSearch array, x
+puts LinearSearch.new.linear_search array, x

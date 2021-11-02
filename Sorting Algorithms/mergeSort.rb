48be4a1fb6f2a501798cb3ab0c35754a668fd8d7
EttoreScolar <ettore.scolar98@gmail.com>
2021-11-01 21:56:08 -0300

    add mergeSort

---
+++ Sorting Algorithms/mergeSort.rb
@@ -0,0 +1,50 @@
+array = [12, 11, 13, 5, 6, 7]
+
+class MergeSort
+    def sort arr, l, r
+        if l < r 
+            m = l + (r-l)/2
+            sort arr, l, m
+            sort arr, m + 1, r
+            merge arr, l, m ,r
+        end
+        arr
+    end
+    def merge arr, l, m, r
+        n1 = m - l + 1
+        n2 = r - m
+        array1 = []
+        array2 = []
+        for i in 0..n1 
+            array1[i] = arr[l + i]
+        end
+        for i in 0..n2
+            array2[i] = arr[m + 1 + i]
+        end
+        i = j = 0
+        k = l 
+        while i < n1 && j < n2 
+            if (array1[i] <= array2[j])
+                arr[k] = array1[i]
+                i += 1        
+            else
+                arr[k] = array2[j]
+                j += 1
+            end
+            k += 1
+        end
+
+        while i < n1 
+            arr[k] = array1[i]
+            i += 1
+            k += 1
+        end
+        while j < n2 
+            arr[k] = array2[j]
+            j += 1
+            k += 1
+        end
+    end
+end
+
+puts MergeSort.new.sort array, 0, array.size - 1

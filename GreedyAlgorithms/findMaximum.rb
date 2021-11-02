45a59bd7a297b2398be5337e0b99653ceedf83e9
EttoreScolar <ettore.scolar98@gmail.com>
2021-11-01 22:14:04 -0300

    refactor findMaximum

---
+++ GreedyAlgorithms/findMaximum.rb
@@ -0,0 +1,37 @@
+
+stack1 = [3,10]
+stack2 = [4,5]
+stack3 = [2,1]
+class FindMaximum
+    def find_maximum arr1, arr2, arr3
+        n1 = arr1.size
+        n2 = arr2.size
+        n3 = arr3.size
+        sum1 = sum2 = sum3 = 0
+        n1.times do |i|
+            sum1+=arr1[i]
+        end
+        n2.times do |i|
+            sum2+=arr2[i]
+        end
+        n3.times do |i|
+            sum3+=arr3[i]
+        end
+
+        return if n1 == 0 || n2 == 0 || n3 == 0
+        top1 = top2 = top3 = 0
+        while true 
+            return sum1 if sum1 == sum2 && sum2 == sum3
+            return if top1 == n1 || top2 == n2 || top3 == n3
+            if sum1 >= sum2 && sum1 >= sum3 
+                sum1 -= arr1[top1+=1]
+            elsif sum2 >= sum1 && sum2 >= sum3 
+                sum2 -= arr2[top2+=1]
+            elsif sum3 >= sum1 && sum3 >= sum2 
+                sum3 -= arr3[top3+=1]
+            end    
+        end
+    end
+end
+
+puts FindMaximum.new.find_maximum stack1, stack2, stack3

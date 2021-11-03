b10f55ad581924a7c9e5bfba66c05eb32a8067dc
EttoreScolar <ettore.scolar98@gmail.com>
2021-11-02 22:26:15 -0300

    refactor

--- String/count.rb
+++ String/count.rb
@@ -1,27 +1,27 @@
 string = '# GeeKs01fOr @ gEEks07'
 
 class Count
-    def countCaracter str 
+  def count_caracter(str)
 
-        maiuscula = 0
-        minuscula = 0
-        numero = 0
-        especial = 0
+    maiuscula = 0
+    minuscula = 0
+    numero = 0
+    especial = 0
 
-        for i in 0..(str.size-1)
-            if str[i].ord >= 65 && str[i].ord <= 90
-                maiuscula += 1
-            elsif str[i].ord >= 97 && str[i].ord <= 122
-                minuscula += 1
-            elsif str[i].ord >= 48 && str[i].ord <= 57
-                numero += 1
-            else
-                especial += 1
-            end
-        end
-
-        puts "Maiuscula: #{maiuscula} \nMinuscula: #{minuscula} \nNumero: #{numero} \nEspecial: #{especial}"
+    (0..(str.size - 1)).each do |i|
+      if str[i].ord >= 65 && str[i].ord <= 90
+        maiuscula += 1
+      elsif str[i].ord >= 97 && str[i].ord <= 122
+        minuscula += 1
+      elsif str[i].ord >= 48 && str[i].ord <= 57
+        numero += 1
+      else
+        especial += 1
+      end
     end
+
+    puts "Maiuscula: #{maiuscula} \nMinuscula: #{minuscula} \nNumero: #{numero} \nEspecial: #{especial}"
+  end
 end
 
-Count.new.countCaracter string
+Count.new.count_caracter string

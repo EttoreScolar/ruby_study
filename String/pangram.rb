1af3d2adb7ae3c8139b2445c86c206f7bcf040d5
EttoreScolar <ettore.scolar98@gmail.com>
2021-11-02 22:28:31 -0300

    refactor

--- String/pangram.rb
+++ String/pangram.rb
@@ -1,19 +1,16 @@
-
-string = " A raposa marrom rápida salta sobre o cachorro"
+string = 'A raposa marrom rápida salta sobre o cachorro'
 class Pangram
-    def pangram str
-        arr = [] 
-        for i in 0..(str.size-1)
-            if str[i].ord >= 65 && str[i].ord <= 90
-                arr.push(str[i].ord)
-            end
-        end
-        if arr.uniq.size == 25
-            return "É Pangram"
-        else
-            return "Não é Pangram"
-        end 
+  def pangram(str)
+    arr = []
+    (0..(str.size - 1)).each do |i|
+      arr.push(str[i].ord) if str[i].ord >= 65 && str[i].ord <= 90
     end
+    if arr.uniq.size == 25
+      'É Pangram'
+    else
+      'Não é Pangram'
+    end
+  end
 end
- 
+
 puts Pangram.new.pangram string

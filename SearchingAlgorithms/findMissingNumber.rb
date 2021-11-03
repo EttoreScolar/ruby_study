08a9920f1f659238cd6a5f9807fd1bd7ab3ae2cb
EttoreScolar <ettore.scolar98@gmail.com>
2021-11-02 21:42:51 -0300

    refactor

--- SearchingAlgorithms/findMissingNumber.rb
+++ SearchingAlgorithms/findMissingNumber.rb
@@ -1,16 +1,15 @@
-
 arr = [1, 2, 4, 6, 3, 7, 8]
 
 class FindMissingNumber
-    def find_missing_number array 
-        array.sort!
-        n = array.size - 1
+  def find_missing_number(array)
+    array.sort!
+    n = array.size - 1
 
-        for i in 0..n 
-           return (i+1) if array[i] != (i+1)
-        end
-        return -1
+    (0..n).each do |i|
+      return (i + 1) if array[i] != (i + 1)
     end
+    -1
+  end
 end
 
 puts FindMissingNumber.new.find_missing_number arr

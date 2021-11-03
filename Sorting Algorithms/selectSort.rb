07e7b01047ed1e6f1ac73bec7773a5cf971e64ff
EttoreScolar <ettore.scolar98@gmail.com>
2021-11-02 22:24:16 -0300

    refactor

--- Sorting Algorithms/selectSort.rb
+++ Sorting Algorithms/selectSort.rb
@@ -1,18 +1,18 @@
 array = [64, 25, 12, 22, 11]
 
 class SelectSort
-    def select_sort arr 
-        n = arr.size - 1
+  def select_sort(arr)
+    n = arr.size - 1
 
-        for i in 0..n
-            min_ind = i
-            for j in (i+1)..n
-                min_ind = j if arr[j] < arr[min_ind]
-            end
-            arr[min_ind], arr[i] = arr[i], arr[min_ind]
-        end
-        arr
+    (0..n).each do |i|
+      min_ind = i
+      ((i + 1)..n).each do |j|
+        min_ind = j if arr[j] < arr[min_ind]
+      end
+      arr[min_ind], arr[i] = arr[i], arr[min_ind]
     end
+    arr
+  end
 end
 
 puts SelectSort.new.selectSort array

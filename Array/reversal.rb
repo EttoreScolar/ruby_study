cf62776c2833fa2375cd3be8d4e9fbc59d359d2a
EttoreScolar <ettore.scolar98@gmail.com>
2021-11-06 21:18:48 -0300

    refactor

--- Array/reversal.rb
+++ Array/reversal.rb
@@ -1,23 +1,23 @@
 arr = [121, 232, 33, 43, 5]
 
-def reverseArray arr, start, e 
+class Reversal
+    def revers_array(arr, start, e)
+        while (start < e)
     
-    while (start < e)
-         
-        arr[start], arr[e] = arr[e], arr[start]
-        start = start + 1
-        e = e - 1
+            arr[start], arr[e] = arr[e], arr[start]
+            start = start + 1
+            e = e - 1
+        end
+        arr
     end
-    arr
+    
+    def right_rotate(arr, d, n)
+        revers_array(arr, 0, n - 1)
+        revers_array(arr, 0, d - 1)
+        revers_array(arr, d, n - 1)
+        arr
+    end
 end
 
-def rightRotate arr, d, n
-     
-    reverseArray(arr, 0, n - 1)
-    reverseArray(arr, 0, d - 1)
-    reverseArray(arr, d, n - 1)
-    arr
-end
-
-puts rightRotate arr, 3, arr.size
+puts Reversal.new.right_rotate(arr, 3, arr.size)
 

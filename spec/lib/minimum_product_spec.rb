697d70586ef6dafb176a1c261afee3771175fd0b
EttoreScolar <ettore.scolar98@gmail.com>
2021-11-07 11:26:05 -0300

    test: add minimumProduct test

---
+++ spec/lib/minimum_product_spec.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require_relative '../../GreedyAlgorithms/minimumProduct'
+describe MinimumProduct do
+  before do
+    # Do nothing
+  end
+
+  after do
+    # Do nothing
+  end
+
+  it 'succeeds' do
+    array = [-1, -1, -2, 4, 3]
+    result = MinimumProduct.new.minimum_product array
+    expect(-24).eql? result
+  end
+end

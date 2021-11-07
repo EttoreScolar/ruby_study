6303a55b6c732445235e97e3bd1e7b427865141f
EttoreScolar <ettore.scolar98@gmail.com>
2021-11-07 11:38:39 -0300

    test: add minimumSum test

---
+++ spec/lib/minimum_sum_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../../GreedyAlgorithms/minimumSum'
+
+describe MinimumSum do
+  before do
+    # Do nothing
+  end
+
+  after do
+    # Do nothing
+  end
+
+
+  it 'succeeds' do
+    a = [2, 3, 4, 5, 4]
+    b = [3, 4, 2, 3, 2]
+    n = a.size - 1
+    k = 3
+    result = MinimumSum.new.min_product a, b, n, k
+    expect(25).eql? result
+  end
+
+end
